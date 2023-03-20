0cee8b0bb8d8f83341e1a6f128c137ca84cea2e9
kondoumh <kondoumh@gmail.com>
2023-03-20 21:05:25 +0900

    Add markdoc support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), react()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react(), markdoc()]
 });
