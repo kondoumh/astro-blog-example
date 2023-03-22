dcf5eee5bf4318abad19ff24ec8de47d72ed26d6
kondoumh <kondoumh@gmail.com>
2023-03-23 07:42:24 +0900

    Add markdoc callout component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,31 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-
 import markdoc from "@astrojs/markdoc";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), markdoc()]
+  integrations: [mdx(), sitemap(), react(),
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+    markdoc({
+      tags: {
+        callout: {
+          render: "Callout",
+          attributes: {
+            type: {
+              type: String
+            },
+            title: {
+              type: String
+            }
+          }
+        }
+      }
+    }
+  ),]
 });
